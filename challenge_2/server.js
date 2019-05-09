13d51c02abbe40db5389bcc888d3bde6ca36bedf
Jianingzhang235 <jianing_chang@hotmail.com>
2019-05-08 22:28:20 -0700

    finshed the server.js

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const port = 3000
+const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+app.use(express.static('client'));
+
+app.post('/upload_json', (req, res) => {
+
+});
+
+$('form').on('submit', function (e) {
+  e.preventDefault();
+  // ... more event handler code here
+});
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,6 @@
+const express = require('express'),
+app = express()
+app.use(express.static('public'))
+app.listen(3000, () =>console.log('port 3000 is awesome!'))
 
 
