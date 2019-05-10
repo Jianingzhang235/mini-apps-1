981e76250ac7382aaa8b99864d9137c3b9866b47
Jianingzhang235 <jianing_chang@hotmail.com>
2019-05-09 23:49:51 -0700

    completed the basic, but i want to prepectify it in the further

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,117 @@
 console.log('hello');
 
+let mark = 'X';
+let moves = 0;
+
+let victoryX = false;
+let victoryO = false;
+
+let board = [];
+
+let space, x, y;
+
+const newGame = () => {
+  mark = 'X';
+  moves = 0;
+  board = [];
+
+  for (var x = 0; x < 3; x++) {
+    // make model row arrays
+    board.push([]);
+
+    for (var y = 0; y < 3; y++) {
+      // populate model rows
+      board[x].push([false, false]); // [is X present, is O present]
+
+      // view
+      let space = document.getElementById(`${x},${y}`);
+      space.addEventListener('click', moveHandler);
+
+      // create text nodes
+      space.innerHTML = '';
+      let text = document.createTextNode('');
+      space.appendChild(text);
+    }
+  }
+};
+
+let button = document.getElementById('button');
+button.addEventListener('click', newGame);
+
+const moveHandler = (e) => {
+  space = e.currentTarget;
+  [x, y] = space.id.split(',');
+
+  if (board[x][y].includes(null)) {
+    alert('SPACE OCCUPIED');
+  // } else if (newGame()) {
+  //   alert('Game Over, Please Start a New Game')
+  } else {
+    moveModel(x, y);
+    moveView(space);
+  }
+};
+
+const moveModel = (x, y) => {
+  board[x][y] = mark === 'X' ? [true, null] : [null, true];
+  moves++;
+  if (moves > 4) { checkWin(); }
+}
+
+const moveView = (space) => {
+  space.textContent = mark;
+  mark === 'X'
+    ? mark = 'O'
+    : mark = 'X';
+}
+
+const checkPlayer = (player) => {
+  let z = player === 'X' ? 0 : 1; // check for Xs or Os
+  let xCount = {};
+  let yCount = {};
+
+  // check columns and rows that player already moved into search rows
+  for (var x = 0; x < 3; x++) {
+    for (var y = 0; y < 3; y++) {
+      // if X played in this row in this column, tick count up
+      if (board[x][y][z]) {
+        xCount[x] === undefined ? xCount[x] = 1 : xCount[x]++;
+        yCount[y] === undefined ? yCount[y] = 1 : yCount[y]++;
+      }
+    }
+  }
+  // victory check
+  if (Object.values(xCount).includes(3) || Object.values(yCount).includes(3)) {
+    return true;
+  }
+
+    // diagonals
+    if (board[1][1][z]) {
+      if (board[0][0][z] && board[2][2][z]) {
+        return true;
+      } else if (board[2][0][z] && board[0][2][z]) {
+        return true;
+      }
+    }
+
+
+  return false; // no win this round
+}
+
+const checkWin = () => {
+  victoryX = checkPlayer('X');
+  victoryO = checkPlayer('O');
+
+  if (victoryX) {
+    setTimeout(() => alert('X WINS'), 10);
+  } else if (victoryO) {
+    setTimeout(() => alert('O WINS'), 10);
+  } else {
+    if (moves === 9) {
+      setTimeout(() => alert('TIE'), 10);
+    }
+  }
+}
+
+
+newGame();
